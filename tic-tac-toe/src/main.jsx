6a2047851f03908efa25975dcb221d7e5235045f
Runju-Raton <68098346+Runju-Raton@users.noreply.github.com>
2024-10-05 00:17:09 +0600

    board done

--- tic-tac-toe/src/main.jsx
+++ tic-tac-toe/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Square from './App.jsx'
+import Board from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Board />
   </StrictMode>,
 )
