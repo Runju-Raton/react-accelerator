8e308b90dabe88407c71b80bb55654aca52b7458
Runju-Raton <68098346+Runju-Raton@users.noreply.github.com>
2024-10-05 09:02:45 +0600

    move tracking and finish the project

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -69,26 +69,48 @@ const Board = ({xIsNext, squares, onPlay}) => {
 export default function Game(){
   const [history,setHistory] = useState([Array(9).fill(null)])
   const [xIsNext,setxIsNext] = useState(true);
-
-  const currentSquares = history[history.length-1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares){
     setxIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0,currentMove+1),nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
   }
+
+  function jumpTo(move){
+    setCurrentMove(move)
+    setxIsNext(move%2===0);
+  }
+
+  const moves = history.map((squares, move)=>{
+    let description;
+    if(move>0){
+      description = `Go to the move # ${move}`;
+    }else{
+      description = `Go to start the game`;
+    }
+    return (
+      <li 
+      key={move}
+      className='bg-gray-700 text-white mb-1 p-1 rounded'>
+        <button onClick={()=>jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return (
-    <div>
-      <div>
+    <div className='flex justify-center '>
+      <div className='mr-16'>
         <Board 
         xIsNext={xIsNext} 
         squares={currentSquares}
         onPlay={handlePlay}></Board>
       </div>
       <div>
-        <ol>
-          {
-            
-          }
+        <ol className='border border-gray-400 p-1 text-lg'>
+          {moves}
         </ol>
       </div>
     </div>
