b835779e05eb94e643e166a588f527956d1d2608
Runju-Raton <68098346+Runju-Raton@users.noreply.github.com>
2024-10-23 08:11:40 +0600

    movie list

--- CineRental/src/App.jsx
+++ CineRental/src/App.jsx
@@ -1,8 +1,18 @@
+import Header from "./Header.jsx";
+import Sidebar from "./Sidebar.jsx";
+import MovieList from "./cine/MovieList.jsx";
+
 function App() {
   return (
-    <>
-
-    </>
+      <>
+          <Header></Header>
+          <main>
+              <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+                <Sidebar></Sidebar>
+                  <MovieList></MovieList>
+              </div>
+          </main>
+      </>
   )
 }
 
